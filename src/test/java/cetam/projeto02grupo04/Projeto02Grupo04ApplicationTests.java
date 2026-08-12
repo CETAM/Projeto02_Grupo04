c90a19b3e2288d78c5bc291c79774e3c9a92d43c
Cleverson S. Sobrinho <cleverson.silva@msn.com>
2026-08-12 15:55:15 -0400

    .

---
+++ src/test/java/cetam/projeto02grupo04/Projeto02Grupo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package cetam.projeto02grupo04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto02Grupo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
