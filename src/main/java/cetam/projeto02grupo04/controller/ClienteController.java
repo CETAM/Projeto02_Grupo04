181a1681ccac0c54b5ff1ce603130c445b864877
zack-reis <261482095+zack-reis@users.noreply.github.com>
2026-08-17 16:39:34 -0400

    Cliente
    Updates nos arquivos de ClienteController e criação do arquivo ClienteRepository.

--- src/main/java/cetam/projeto02grupo04/controller/ClienteController.java
+++ src/main/java/cetam/projeto02grupo04/controller/ClienteController.java
@@ -1,15 +1,58 @@
 package cetam.projeto02grupo04.controller;
 
+import cetam.projeto02grupo04.model.Cliente;
+import cetam.projeto02grupo04.repository.ClienteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
-    // Acessa a página de gerenciamento de clientes em http://localhost:8080/clientes
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+
+    // A página começa em 0 e mostra 5 clientes por página.
     @GetMapping
-    public String carregarClientes() {
+    public String carregarClientes(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            Model model) {
+
+        Pageable pageable = PageRequest.of(
+                page,
+                size,
+                Sort.by("nome").ascending()
+        );
+
+        Page<Cliente> clientes = clienteRepository.findAll(pageable);
+
+        model.addAttribute("clientes", clientes);
+        model.addAttribute("cliente", new Cliente());
+
         return "clientes/clientes";
+    }
+
+    // Cadastra um novo cliente.
+    @PostMapping("/salvar")
+    public String salvarCliente(@ModelAttribute Cliente cliente) {
+        clienteRepository.save(cliente);
+        return "redirect:/clientes";
+    }
+
+    // Exclui um cliente pelo ID.
+    @GetMapping("/excluir/{id}")
+    public String excluirCliente(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+        return "redirect:/clientes";
     }
 }
