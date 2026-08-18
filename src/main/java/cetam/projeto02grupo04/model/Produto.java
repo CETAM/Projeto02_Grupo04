8afd39f89faa9469c2a2abe785d2f62446ecdcb0
nascimentosuely392-netizen <nascimentosuely392@gmail.com>
2026-08-18 14:50:52 -0400

    Model
    Adicionado model de Pedido e Produto.

--- src/main/java/cetam/projeto02grupo04/model/Produto.java
+++ src/main/java/cetam/projeto02grupo04/model/Produto.java
@@ -1,40 +1,32 @@
-package cetam.projeto02grupo04.model;
 
+package cetam.projeto02grupo04.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
-
 
 @Entity
 @Table(name = "templates")
 public class Produto {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(nullable = false, length = 150)
     private String nome;
-
 
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal preco;
 
-
     @Column(name = "quantidade_estoque", nullable = false)
     private Integer quantidadeEstoque;
-
 
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
-
     public Produto() {
     }
-
 
     public Produto(Long id, String nome, BigDecimal preco, Integer quantidadeEstoque, Categoria categoria) {
         this.id = id;
@@ -44,51 +36,41 @@ public class Produto {
         this.categoria = categoria;
     }
 
-
     public Long getId() {
         return id;
     }
-
 
     public void setId(Long id) {
         this.id = id;
     }
 
-
     public String getNome() {
         return nome;
     }
-
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-
     public BigDecimal getPreco() {
         return preco;
     }
-
 
     public void setPreco(BigDecimal preco) {
         this.preco = preco;
     }
 
-
     public Integer getQuantidadeEstoque() {
         return quantidadeEstoque;
     }
-
 
     public void setQuantidadeEstoque(Integer quantidadeEstoque) {
         this.quantidadeEstoque = quantidadeEstoque;
     }
 
-
     public Categoria getCategoria() {
         return categoria;
     }
-
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
