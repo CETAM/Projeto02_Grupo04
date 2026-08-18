87afe89ef859cfa49a56aab59690386434380819
suelentavares <94622266+suelentavares@users.noreply.github.com>
2026-08-18 13:50:49 -0400

    correção
    foi atualizado

--- src/main/java/cetam/projeto02grupo04/repository/ProdutoRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/ProdutoRepository.java
@@ -4,6 +4,13 @@ import cetam.projeto02grupo04.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+import cetam.projeto02grupo04.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cetam.projeto02grupo04.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
