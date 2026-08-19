61540bcabd2bd7a94fb93bb5f2c0149a38e2cb87
zack-reis <261482095+zack-reis@users.noreply.github.com>
2026-08-19 16:01:58 -0400

    Repository Pessoa
    Criação do Repository Pessoa

--- src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
@@ -1,10 +1,12 @@
-package com.loja.roupas.repository;
+package cetam.projeto02grupo04.repository;
 
-import com.loja.roupas.model.Pessoa;
+import cetam.projeto02grupo04.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
 
+    List<Pessoa> findByTipoIgnoreCase(String tipo);
+
+}
