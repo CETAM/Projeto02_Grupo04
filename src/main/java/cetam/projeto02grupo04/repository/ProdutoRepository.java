28c4be920a5c23eb90a0f35c4b91f8be7cfec67e
Suely Nascimento <nascimentosuely392@gmail.com>
2026-08-20 03:52:55 -0400

    Corrigindo conflito no ProdutoRepository
    Removendo imports duplicados e limpando marcadores de conflito.

--- src/main/java/cetam/projeto02grupo04/repository/ProdutoRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/ProdutoRepository.java
@@ -4,16 +4,6 @@ import cetam.projeto02grupo04.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
-=======
-
-import cetam.projeto02grupo04.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import cetam.projeto02grupo04.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
->>>>>>> 84691fad22a72bea5045625f5315b71a34dd4de7
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
