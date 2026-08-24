cb3d4d06046a9308d61a66a997a2b9299c148e78
Sophia Roque Martins <martinssophia983@gmail.com>
2026-08-24 16:14:15 -0300

    Alteracoes em repository endereco (#73)

--- src/main/java/cetam/projeto02grupo04/repository/EnderecoRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/EnderecoRepository.java
@@ -1,10 +1,11 @@
+
 package cetam.projeto02grupo04.repository;
 
+import cetam.projeto02grupo04.model.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import cetam.projeto02grupo04.model.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
 }
+
