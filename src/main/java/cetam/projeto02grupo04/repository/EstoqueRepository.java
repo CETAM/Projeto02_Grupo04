aa888f733bde86698ad160191c9b385251533b16
Sophia Roque Martins <martinssophia983@gmail.com>
2026-08-25 14:39:02 -0400

    alteraçoes em repository estoque

--- src/main/java/cetam/projeto02grupo04/repository/EstoqueRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/EstoqueRepository.java
@@ -3,5 +3,5 @@ package cetam.projeto02grupo04.repository;
 import cetam.projeto02grupo04.model.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
+public interface EstoqueRepository extends JpaRepository<Estoque, Integer> {
 }
