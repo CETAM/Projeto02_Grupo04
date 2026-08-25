82208ed27b74d90e82c5ed42b529bc6cf42d7080
Sophia Roque Martins <martinssophia983@gmail.com>
2026-08-25 16:05:39 -0400

    alter repository pessoa

--- src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
@@ -2,8 +2,6 @@ package cetam.projeto02grupo04.repository;
 
 import cetam.projeto02grupo04.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-    List<Pessoa> findByTipoIgnoreCase(String tipo);
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 }
