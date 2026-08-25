8a4347c3af3e79cc07e99ee70006ec629129886c
zack-reis <261482095+zack-reis@users.noreply.github.com>
2026-08-25 16:39:55 -0400

    Relatorio do dia 25/08

--- src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
+++ src/main/java/cetam/projeto02grupo04/repository/PessoaRepository.java
@@ -4,6 +4,7 @@ import cetam.projeto02grupo04.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface PessoaRepository
+        extends JpaRepository<Pessoa, Long> {
     List<Pessoa> findByTipoIgnoreCase(String tipo);
 }
